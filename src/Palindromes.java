f6bc740a4a308019a8a70f07baae6582149a198b
Joshua-B <joshua_bloom96@yahoo.co.uk>
2021-11-11 17:11:12 +0000

    initial commit

---
+++ src/Palindromes.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Palindromes {
+
+    ArrayList<String> words = new ArrayList<>() {{
+        add("bib");
+        add("no");
+        add("ok");
+        add("racecar");
+        add("winner");
+        add("chicken");
+        add("robot1");
+        add("ok");
+        add("kebab");
+        add("nun");
+        add("madam");
+        add("java");
+        add("computer");
+        add("civic");
+        add("spitfire");
+        add("coffee");
+        add("hannah");
+        add("deified");
+        add("sword");
+        add("pilgrim");
+        add("level");
+    }};
+
+
+
+}
